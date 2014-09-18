fa9b756bb66cc72e5bd4c26795e333a83e489560
Colin.Graham <grahamcolin14@gmail.com>
2014-09-18 11:50:48 -0600

    initial commit

---
+++ Battleship/src/Battleship.java
@@ -0,0 +1,11 @@
+
+public class Battleship
+	{
+
+	public static void Battleship()
+		{
+			System.out.println("Welcome soldier, /n Today your skills will be tested against those dirty communist's battle ship");
+
+		}
+
+	}
